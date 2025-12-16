1d1c8772e414c79271523d4c99b59d1b87327c9b
ajju <150140351+Arvind05092005@users.noreply.github.com>
2025-12-16 14:55:26 +0530

    login signup

--- frontend/backend/server.js
+++ frontend/backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import LoginRoutes from './routes/login.js';
+import SiginupRoutes from './routes/signup.js';
 const app=express();
 app.use(express.json());
 
 
+app.use('/api/login',LoginRoutes);
+app.use('/api/signup',SiginupRoutes);
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
