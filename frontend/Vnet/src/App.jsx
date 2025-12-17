4f88abc0ec60e78427d65d63ef729aabfd0f856a
Utkarsh Raj <utkarshumang111@gmail.com>
2025-12-17 17:53:39 +0530

    Add animated login, signup and forgot password flow

--- frontend/Vnet/src/App.jsx
+++ frontend/Vnet/src/App.jsx
@@ -1,28 +1,40 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
 
 import Login from './components/Login'
 import Scroller from './components/Scroller'
 import NavBar from './components/NavBar'
-//import Profile from './components/Dashboard'
 import Dashboard from './components/Dashboard'
+import Signup from "./components/Signup";
+
+function Layout() {
+  const location = useLocation();
+
+  // Hide NavBar on Login page
+  const hideNavBar = location.pathname === "/Login";
+
+  return (
+    <div className="bg-[#0A0A0A] w-screen h-screen text-[#E5E7EB]">
+      
+      {!hideNavBar && <NavBar isVisible={true} />}
+
+      <Routes>
+        <Route path="/" element={<Scroller />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Signup" element={<Signup />} />
+      </Routes>
+
+    </div>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="bg-[#0A0A0A] w-screen h-screen text-[#E5E7EB]">
-        
-        <NavBar isVisible={true} />
-
-        <Routes>
-          <Route path="/" element={<Scroller />} />
-          <Route path="/Dashboard" element={<Dashboard />} />
-          <Route path="/Login" element={<Login />} />
-        </Routes>
-
-      </div>
+      <Layout />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
